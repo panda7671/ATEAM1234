1b35c0061fd8ad7079b1861cd3aa244b8ef23820
panda7671 <7671hoon@gmail.com>
2025-05-09 23:31:31 +0900

    Initial Unity project commit

---
+++ Assets/script/GPTResponse.cs
@@ -0,0 +1,20 @@
+using System;
+
+[System.Serializable]
+public class GPTResponse
+{
+    public Choice[] choices;
+}
+
+[System.Serializable]
+public class Choice
+{
+    public Message message;
+}
+
+[System.Serializable]
+public class Message
+{
+    public string role;
+    public string content;
+}
