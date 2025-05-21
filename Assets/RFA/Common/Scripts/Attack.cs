8d3d2bf2756c985b6b51fd7e95272703bd2606cf
wjstn <wjstn@example.com>
2025-05-21 22:58:32 +0900

    안뇽

---
+++ Assets/RFA/Common/Scripts/Attack.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Retro.ThirdPersonCharacter
+{
+    public class Attack : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
+
